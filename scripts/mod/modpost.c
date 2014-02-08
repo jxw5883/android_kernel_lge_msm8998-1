32ff24e36c56d21c327f797600257ac86d4c8255
Andi Kleen <ak@linux.intel.com>
2014-02-08 09:01:11 +0100

    Kbuild, lto, workaround: Don't warn for initcall_reference in modpost
    This reference is discarded, but can cause warnings when it refers to
    exit. Ignore for now.
    
    This is a workaround and can be removed once we get rid of
    -fno-toplevel-reorder
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Link: http://lkml.kernel.org/r/1391846481-31491-7-git-send-email-ak@linux.intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1691,6 +1691,28 @@ static void check_section_mismatch(const char *modname, struct elf_info *elf,
 		else
 			default_mismatch_handler(modname, elf, mismatch,
 						 r, sym, fromsec);
+		Elf_Sym *to;
+		Elf_Sym *from;
+		const char *tosym;
+		const char *fromsym;
+
+		from = find_elf_symbol2(elf, r->r_offset, fromsec);
+		fromsym = sym_name(elf, from);
+		to = find_elf_symbol(elf, r->r_addend, sym);
+		tosym = sym_name(elf, to);
+
+		if (!strncmp(fromsym, "reference___initcall",
+				sizeof("reference___initcall")-1))
+			return;
+
+		/* check whitelist - we may ignore it */
+		if (secref_whitelist(mismatch,
+					fromsec, fromsym, tosec, tosym)) {
+			report_sec_mismatch(modname, mismatch,
+			   fromsec, r->r_offset, fromsym,
+			   is_function(from), tosec, tosym,
+			   is_function(to));
+		}
 	}
 }
 
