9980773e5dfee721f8aa379d7f275e803962f439
Andi Kleen <ak@linux.intel.com>
2014-02-08 09:01:13 +0100

    Kbuild, lto: add ld-version and ld-ifversion macros
    To check the linker version. Used by the LTO makefile.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Link: http://lkml.kernel.org/r/1391846481-31491-9-git-send-email-ak@linux.intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- scripts/ld-version.sh
+++ scripts/ld-version.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/awk -f
 # extract linker version number from stdin and turn into single number
 	{
+<<<<<<< HEAD
 	gsub(".*\\)", "");
+=======
+	gsub(".*)", "");
+>>>>>>> ccbef1674a15... Kbuild, lto: add ld-version and ld-ifversion macros
 	split($1,a, ".");
 	print a[1]*10000000 + a[2]*100000 + a[3]*10000 + a[4]*100 + a[5];
 	exit
