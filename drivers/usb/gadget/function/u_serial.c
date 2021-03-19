d9a1e4427f4b9a1f911537b42feac377bc58e4fd
lllgts <wuer031@126.com>
2021-03-19 21:42:04 +0800

    Fix build error of 'u_serial.c'

--- drivers/usb/gadget/function/u_serial.c
+++ drivers/usb/gadget/function/u_serial.c
@@ -793,7 +793,6 @@ static int gs_start_io(struct gs_port *port)
 #else
 	if (started && port->port.tty) {
 #endif
-	if (started) {
 		gs_start_tx(port);
 		/* Unblock any pending writes into our circular buffer, in case
 		 * we didn't in gs_start_tx() */
