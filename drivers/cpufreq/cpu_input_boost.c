bb042bda225ef07b3339c3f2743c6b055b054310
lllgts <wuer031@126.com>
2021-06-06 21:13:38 +0800

    Revert "cpu_input_boost: Perform GPU boost for POWERHAL_MAX_BOOST"
    This reverts commit 7563b9c20ee7aae06fff127153fadd44ad8feaac.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -325,7 +325,16 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Do powerhal boost for powerhal_max_boost */
 	if (test_bit(POWERHAL_MAX_BOOST, &b->state)) {
-		/* Do nothing for now */
+		/* Disable EAS behaviour */
+		energy_aware_enable = false;
+
+		/* CPUBW boost */
+		set_hyst_trigger_count_val(0);
+		set_hist_memory_val(0);
+		set_hyst_length_val(0);
+	} else {
+		/* Enable EAS behaviour */
+		energy_aware_enable = true;
 	}
 
 	/* return early if being max bosted */
