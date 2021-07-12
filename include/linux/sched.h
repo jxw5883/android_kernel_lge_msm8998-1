19323cbfc421928b0a1f1fd2e4349e7dd54b85ca
lllgts <wuer031@126.com>
2021-07-12 20:58:20 +0800

    Revert "sched: Add API to migrate the current process to a given cpumask"
    This reverts commit 9463a6ab61fb8fd7d69c297a50938fb30ad2bbea.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3043,15 +3043,6 @@ static inline int thread_group_empty(struct task_struct *p)
 	return list_empty(&p->thread_group);
 }
 
-void sched_migrate_to_cpumask_start(struct cpumask *old_mask,
-				    const struct cpumask *dest);
-void sched_migrate_to_cpumask_end(const struct cpumask *old_mask,
-				  const struct cpumask *dest);
-
-#ifndef cpu_relax_yield
-#define cpu_relax_yield() cpu_relax()
-#endif
-
 #define delay_group_leader(p) \
 		(thread_group_leader(p) && !thread_group_empty(p))
 
