09c14dc4bbfd6f702e91732671df45deb48a793a
lllgts <wuer031@126.com>
2021-07-17 21:13:10 +0800

    kernel:sched:Fix build error

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -109,8 +109,3 @@ SCHED_FEAT(FBT_STRICT_ORDER, true)
  * RT class.
  */
 SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, true)
-
-/*
- * UtilEstimation. Use estimated CPU utilization.
- */
-SCHED_FEAT(UTIL_EST, true)
